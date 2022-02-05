7c08dedcdd263154fee0ab59407b74e24448bb7b
alkaitagi <alkaitagi@outlook.com>
2022-02-05 22:46:04 +0300

    Clean up

--- lib/modules/play/screens/play.dart
+++ lib/modules/play/screens/play.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:wordle/modules/play/models/input_key.dart';
 import 'package:wordle/modules/play/screens/help.dart';
 import 'package:wordle/modules/play/utils.dart';
