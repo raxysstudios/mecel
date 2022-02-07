c0431d5189466535bcb605e6ec27dd9636cb2411
alkaitagi <alkaitagi@outlook.com>
2022-02-07 12:30:41 +0300

    Clean up

--- lib/modules/settings/screens/settings.dart
+++ lib/modules/settings/screens/settings.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:wordle/shared/extensions.dart';
 import 'package:wordle/shared/models/game_state.dart';
@@ -18,8 +17,6 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final tr = context.read<Map<String, String>>();
-    print('STNG LCLZ $tr');
     return Scaffold(
       appBar: AppBar(
         leading: const RoundedBackButton(),
